40290d009c9d55aaa73018ec6867c2200480d41e
Alyssa Watson <alyssa.s.watson@gmail.com>
2018-03-07 20:33:37 +0000

    automatically backed up by learn

---
+++ app/models/heros.rb
@@ -0,0 +1,14 @@
+class Heros
+
+  attr_accessor :name, :power, :bio
+
+  @@all = []
+
+  def initialize(data)
+    @name = data[:name]
+    @power = data[:power]
+    @bio = data[:bio]
+    @@all << self
+  end
+
+end
